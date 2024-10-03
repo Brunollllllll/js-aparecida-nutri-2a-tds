6fcb0c7d9d626c47255f1cec28f58ce404148c56
bruno.potier <160505482+Brunollllllll@users.noreply.github.com>
2024-10-03 08:05:21 -0300

    Atualização_v1

--- js/principal.js
+++ js/principal.js
@@ -14,26 +14,22 @@ var tdImc = paciente.querySelector(".info-imc");
 var alturaEhValida = true;
 var pesoEhValido = true;
 
-if (peso <= 0 || peso > 1000) {
+if (peso <= 0 || peso >= 1000){
     console.log("Peso inválido!");
     tdPeso.textContent = "Peso inválido!";
     pesoEhValido = false;
 
 }
 
-if (altura <= 0 || altura >= 3) {
+if (altura <= 0 || altura >= 3.00){
     console.log("Altura inválida!");
     tdAltura.textContent = "Altura inválida!";
     alturaEhValida = false;
 }
 
-if (alturaEhValida && pesoEhValido) {
-
+if (alturaEhValida && pesoEhValido){
     var imc = peso / (altura * altura);
     tdImc.textContent = imc;    
 } else {
     tdImc.textContent = "Altura e/ou peso inválidos!"
 }
-
-
-
